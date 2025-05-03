5ed2f4788c3822ff6fe294ed4ad69828afb8c19f
Lowland <122749572+lowlandrepair@users.noreply.github.com>
2025-05-03 05:44:58 -0700

    js

---
+++ main.js
@@ -0,0 +1,41 @@
+
+
+
+var x = 5;
+var y = 10; 
+var sum = x + y; 
+console.log("The sum of x and y is: " + sum); 
+var bool = true;
+var str = "Hello, World!";
+if (bool == true) {
+    console.log("you have to work");
+}
+else {
+    console.log("you are not working");
+}
+
+var p = 20;
+var q = p ** 2;
+console.log(q);
+
+var m = 15;
+m += 5; // 15 + 5 = 20
+console.log(m);
+
+var r = 35;
+r *= 5;
+console.log(r);
+
+var num1 = 5;
+var num2 = 10;
+var num3 = 5;
+console.log(num1 != num2); // true
+console.log(num1 == num3); // true
+console.log(num1 > num3);  // false
+console.log(num1 >= num3); // true
+console.log(num1 < num2);  // true
+console.log(num1 <= num2); // true
+if (num1 != num2) {
+    console.log("your bitcoin has been sent"); 
+}
+
